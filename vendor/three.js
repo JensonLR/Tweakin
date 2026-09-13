84dc3d2f3247ef68fb8aea80639e0c2f35e965bc
JensonLR <jensonlewis0@gmail.com>
2026-09-13 10:11:44 +0100

    Add Three.js runtime adapter

---
+++ vendor/three.js
@@ -0,0 +1,3 @@
+// Three.js runtime loaded as a standards-based ESM dependency.
+import * as THREE_NS from 'https://cdn.jsdelivr.net/npm/three@0.186.0/build/three.module.js';
+export const THREE = THREE_NS;
